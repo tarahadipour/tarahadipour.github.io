14bcc9d3a32c91a5f2bb21ed0100a60493525b82
Tara <tarahadipour@berkeley.edu>
2018-11-28 20:45:30 -0800

    started a website

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+
+	const distanceToNextImage = -1000;
+	let currentImageNumber = 0;
+	// $(".carousel-section").click(function() {
+	// 	let index = $(this).attr("id");
+	// 	let parsed = parseInt(index)*distanceToNextImage + "px";
+	// 	$("#carousel-strip").css("left", parsed)
+	// });
+
+
+
+	$("#right").click(function() {
+		console.log("right")
+		currentImageNumber = currentImageNumber + 1;
+		$("#carousel-strip").css("left", currentImageNumber*distanceToNextImage);
+	});
+		console.log("right")
+	$("#left").click(function() {
+		currentImageNumber = currentImageNumber - 1;
+		$("#carousel-strip").css("left", currentImageNumber*distanceToNextImage);
+	});
+
+	$(".downarrows").click(function(){
+		console.log("down")
+		$(window).scrollTo($(".postcontent"))
+	});
+
+	$(".backtotop").click(function(){
+		console.log("down")
+		$(window).scrollTo($(".contentarea"))
+	});
+
+	$.extend($.scrollTo.defaults, {
+  axis: 'y',
+  duration: 400
+});
+
+
+
+})
